bba8e76e4b8cb65c23fd9a532add37d2431c6c96
emilygrochoski <140463370+emilygrochoski@users.noreply.github.com>
2024-10-22 22:01:55 -0300

    aula 22/10 2

---
+++ missaoVisaoValores.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Missão',
+    'Saiba qual é a missão de nossa empresa!',
+    'Nossa missão é oferecer equipamentos seguros e verificados corretamente.'
+)
+
+criaCartao (
+    'Visão',
+    'Saiba qual é a visão de nossa empresa!',
+    'Nossa visão é crescer financeiramente, até conseguir criar mais unidades de loja em diferentes localizações.'
+)
+
+criaCartao (
+    'Valores',
+    'Saiba quais são os valores de nossa empresa!',
+    'Nossos valores são solidariedade, transparência, equidade,excelência, inovação.'
+) 
